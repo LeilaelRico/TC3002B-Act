c7d5d3f3328974158b635eba7529a0a146a4dbc1
Leilael Rico <A01707023@tec.mx>
2024-04-15 17:33:03 -0600

    Routes updated.

--- M3_LexerLogo/main.py
+++ M3_LexerLogo/main.py
@@ -7,7 +7,7 @@ from Lexer import *
 import sys
 
 if __name__ == '__main__':
-	lexer = Lexer(".\\M4_LexerLogo\\test_cases\\prog1.txt")
+	lexer = Lexer(".\\M3_LexerLogo\\test_cases\\prog1.txt")
 	
 	token = lexer.scan()
 	while token.getTag() != Tag.EOF:
