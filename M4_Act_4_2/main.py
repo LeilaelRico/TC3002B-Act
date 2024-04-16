c9996730083b51dab3074f4a6764fbe3fd9a4cf6
Leilael Rico <A01707023@tec.mx>
2024-04-15 18:03:21 -0600

    Added Files / Act4.2 started
    Co-authored-by: Fabián Avilés<feacfabian@gmail.com>

--- M4_Act_4_2/main.py
+++ M4_Act_4_2/main.py
@@ -3,3 +3,26 @@
 * Cristian Rico
 * Actividad 4.2: Método Lineal Congruencial y Pruebas de Aleatoriedad
 """
+
+def lcm(X0, a, c, m, n):
+    numbers = []
+    X = X0
+    for _ in range(n):
+        X = (a * X + c) % m
+        numbers.append(X / m)
+    print(numbers)
+    return numbers
+
+# def chi():
+
+# def runs():
+
+
+if __name__ == '__main__':
+    X0 = 6
+    a = 32
+    c = 3
+    m = 80
+    n = 10
+
+    lcm(X0, a, c, m, n)
